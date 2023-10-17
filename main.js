91dcc926a074d82a734842c68a959322f54522cb
iriscorn <korniienko.iris@gmail.com>
2023-10-17 21:30:55 +0300

    added html and js files

---
+++ main.js
@@ -0,0 +1,7 @@
+let firstNumb = +prompt('Enter the first number');
+let secondNumb = +prompt('Enter the second number');
+let thirdNumb = +prompt('Enter the third number');
+
+let average = (firstNumb + secondNumb + thirdNumb)/3;
+
+alert('(' + firstNumb + ' + ' + secondNumb + ' + ' + thirdNumb + ') / 3 = ' + average + '\n Your average is ' + average);
